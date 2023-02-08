95044f59f1a42f0012afb1935068fa8def53a53c
n4n0byt3 <shivbo1@hotmail.com>
2023-02-08 11:59:04 +0000

    completed airport classes tasks

--- Bag.js
+++ Bag.js
@@ -1,8 +1,18 @@
 class Bag {
-    // Include constructor Method with the following property: 
+    constructor (weight) {
+        if (!weight) {
+            throw new Error("bag needs weight")
+        }
+        this.weight = weight;
+    }
         // 1. Weight
             //1a. Should handle an exception in case user adds no weight and throw an error.
   
 }
+try {
+    const bag = new Bag();
+} catch(err) {
+    console.log(err)
+}
 
-//Export the module
+module.exports = Bag;
